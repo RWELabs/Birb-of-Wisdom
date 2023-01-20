509cf560e988df4b3992ad4d1a6547392cf31fa7
Ryan Walpole <ryan.walpole@outlook.com>
2023-01-20 14:08:20 +1100

    Add UI
    Add user interface with Windows Forms
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Source Code/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Birb_of_Wisdom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Question = new System.Windows.Forms.TextBox();
+            this.Query = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Answer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 494);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(96, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Check for Updates";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.Click += new System.EventHandler(this.CheckUpdate);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(273, 493);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(125, 16);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "v1.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(357, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "The Birb of Wisdom invites you to ask a yes or no question.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Type your question in the text box below.";
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(12, 64);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(286, 20);
+            this.Question.TabIndex = 7;
+            this.Question.Click += new System.EventHandler(this.Question_Click);
+            this.Question.TextChanged += new System.EventHandler(this.Question_TextChanged);
+            this.Question.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Question_KeyDown);
+            // 
+            // Query
+            // 
+            this.Query.Location = new System.Drawing.Point(304, 62);
+            this.Query.Name = "Query";
+            this.Query.Size = new System.Drawing.Size(98, 23);
+            this.Query.TabIndex = 8;
+            this.Query.Text = "Query the Birb";
+            this.Query.UseVisualStyleBackColor = true;
+            this.Query.Click += new System.EventHandler(this.Query_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.BackColor = System.Drawing.Color.White;
+            this.Answer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Answer.Location = new System.Drawing.Point(12, 96);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(389, 389);
+            this.Answer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Answer.TabIndex = 0;
+            this.Answer.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 519);
+            this.Controls.Add(this.Query);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Answer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(430, 558);
+            this.MinimumSize = new System.Drawing.Size(430, 558);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Birb of Wisdom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Answer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Answer;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Question;
+        private System.Windows.Forms.Button Query;
+    }
+}
+
---
+++ Source Code/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Birb_of_Wisdom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "v" + Properties.Settings.Default.Version;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckUpdate(object sender, EventArgs e)
+        {
+            linkLabel1.Text = "Checking for Updates...";
+            XmlDocument document = new XmlDocument();
+            document.Load("https://www.ryanwalpole.com/h/wisdombirb/updatecheck.xml");
+            string allText = document.InnerText;
+            if (allText.Contains(Properties.Settings.Default.Version))
+            {
+                linkLabel1.Text = "Updates Available!";
+                DialogResult dr = MessageBox.Show("There is an update available for the Birb of Wisdom. Would you like to update now?", "Birb of Wisdom", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        string dataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                        string updatelocation = Path.Combine(dataPath, "wisdombirblatest.exe");
+                        progressBar1.Visible = true;
+                        linkLabel1.Text = "Downloading Updates...";
+
+                        wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                        wc.DownloadFileAsync(
+                            // Param1 = Link of file
+                            new System.Uri("https://www.ryanwalpole.com/h/wisdombirb/latest.exe"),
+                            // Param2 = Path to save
+                            updatelocation
+                        );
+                        this.BringToFront();
+                    }
+                }
+            }
+            else if (!allText.Contains(Properties.Settings.Default.Version))
+            {
+                linkLabel1.Text = "No updates available :)";
+            }
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            long FileSize = e.TotalBytesToReceive / 1024;
+            //DLPercentText.Text = e.ProgressPercentage.ToString() + "% of " + FileSize.ToString() + " kb";
+            //DLPercentText.Text = e.ProgressPercentage.ToString() + "% of " + e.TotalBytesToReceive.ToString() + " bytes";
+
+            if (progressBar1.Value == 100)
+            {
+                this.BringToFront();
+                progressBar1.Value = 100;
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string dataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string updatelocation = Path.Combine(dataPath, "wisdombirblatest.exe");
+
+            Process.Start(updatelocation);
+        }
+
+        private void Question_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //Do something
+                e.Handled = true;
+                Query.PerformClick();
+            }
+        }
+
+        private void Query_Click(object sender, EventArgs e)
+        {
+            //List
+            //1 - Yes
+            //2 - No
+            //3 - I believe so
+            //4 - Unfortunately Yes
+            //5 - You already know
+            //6 - If you have to ask, no
+            //7 - Definitely not.
+            //8 - Be careful what you ask for
+            //9 - Ask again later
+
+            if (Question.Text == string.Empty)
+            {
+                //do nothing
+            }
+            else
+            {
+                Random rnd = new Random();
+                //string[] secOptions = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+                int randomNumber = rnd.Next(1, 10);
+                //MessageBox.Show(randomNumber.ToString());
+                Query.Text = "Ask Again";
+
+                switch (randomNumber)
+                {
+                    case 1:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer1);
+                        break;
+                    case 2:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer2);
+                        break;
+                    case 3:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer3);
+                        break;
+                    case 4:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer4);
+                        break;
+                    case 5:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer5);
+                        break;
+                    case 6:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer6);
+                        break;
+                    case 7:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer7);
+                        break;
+                    case 8:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer8);
+                        break;
+                    case 9:
+                        Answer.Image = new Bitmap(Properties.Resources.Answer9);
+                        break;
+                }
+            }
+        }
+
+        private void Question_TextChanged(object sender, EventArgs e)
+        {
+            if(Query.Text == "Query the Birb")
+            {
+                //do nothing
+            }
+            else
+            {
+                Query.Text = "Query the Birb";
+            }
+        }
+
+        private void Question_Click(object sender, EventArgs e)
+        {
+            Question.SelectAll();
+        }
+    }
+}
