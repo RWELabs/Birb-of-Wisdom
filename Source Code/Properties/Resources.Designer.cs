f462aef93c902aca3b0bcf5b0bd83a875d83857c
Ryan Walpole <ryan.walpole@outlook.com>
2023-01-25 10:03:10 +1100

    Add Images to Resources
    Add the new answer images to the resources file.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- Source Code/Properties/Resources.Designer.cs
+++ Source Code/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Birb_of_Wisdom.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,86 @@ namespace Birb_of_Wisdom.Properties {
         internal static System.Drawing.Bitmap Answer1 {
             get {
                 object obj = ResourceManager.GetObject("Answer1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer10 {
+            get {
+                object obj = ResourceManager.GetObject("Answer10", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer11 {
+            get {
+                object obj = ResourceManager.GetObject("Answer11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer12 {
+            get {
+                object obj = ResourceManager.GetObject("Answer12", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer13 {
+            get {
+                object obj = ResourceManager.GetObject("Answer13", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer14 {
+            get {
+                object obj = ResourceManager.GetObject("Answer14", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer15 {
+            get {
+                object obj = ResourceManager.GetObject("Answer15", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer16 {
+            get {
+                object obj = ResourceManager.GetObject("Answer16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Answer17 {
+            get {
+                object obj = ResourceManager.GetObject("Answer17", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
